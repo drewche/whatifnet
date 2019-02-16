8532c108a695b7d05f2356a167f11d17cc5fa9a8
Frank Pang <fpang@usc.edu>
2019-02-16 09:33:20 -0800

    react

---
+++ src/App.js
@@ -0,0 +1,26 @@
+import React, { Component } from 'react';
+import logo from './resources/images/logo.svg';
+import './resources/styles/App.css';
+import Home from './Home.js';
+import { Button } from 'react-bootstrap';
+
+class App extends Component {
+  constructor(props) {
+      super(props);
+  }
+
+  render() {
+    
+    return (
+      <div className>
+        <Home message="Hello World!!!"
+              color="blue"/>
+        <Home message="Bye!"
+              color="red"/>
+        <Button variant="secondary">Secondary</Button>
+      </div>
+    );
+  }
+}
+
+export default App;
