46294d3791820affdd701a14f4a8d22a4d4c9563
Frank Pang <fpang@usc.edu>
2019-02-16 22:49:15 -0800

    final as of now

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import './resources/App.css';
 import Home from './components/Home.js';
 import ArcMap from './components/ArcMap.js';
 import Results from './components/Results.js'
+import LineGraph from './components/LineGraph.js'
 
 class App extends Component {
   constructor(props) {
@@ -14,8 +15,8 @@ class App extends Component {
       <div className="fullscreen">
         <Home message="What if... global warming was at your doorstep?"
               color="black"/>
-        <ArcMap ref={this.section1} />
-        <Results ref={this.section2} />
+        <ArcMap />  
+        <Results />
       </div>
     );
   }
---
+++ src/components/LineGraph.js
@@ -0,0 +1,19 @@
+import React, { Component } from 'react';
+import '../resources/Results.css';
+
+class LineGraph extends Component {
+
+	constructor(props) {
+		super(props);
+	}
+
+	render() {
+		return (
+			<div className="line-graph-component-container">
+				Hello
+			</div>
+		);
+	}
+}
+
+export default LineGraph;
