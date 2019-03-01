5c838ff8c3648201765841b55d3a8613da5871e5
Andrew Che <andrew.che0122@gmail.com>
2019-02-28 20:23:05 -0800

    Deploy Heroku

---
+++ build/precache-manifest.6c8c9d864abe057f5b87788ea432a916.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "6cb1921fca7f3b4fdfbc015e9b191b08",
+    "url": "/static/media/wildfire.6cb1921f.jpg"
+  },
+  {
+    "revision": "fdfcfda2d9b1bf31db52",
+    "url": "/static/js/runtime~main.fdfcfda2.js"
+  },
+  {
+    "revision": "ed547a7c4f24cb826742",
+    "url": "/static/js/main.ed547a7c.chunk.js"
+  },
+  {
+    "revision": "2d1267ffdf5bff27b970",
+    "url": "/static/js/2.2d1267ff.chunk.js"
+  },
+  {
+    "revision": "ed547a7c4f24cb826742",
+    "url": "/static/css/main.1de920ce.chunk.css"
+  },
+  {
+    "revision": "7a01c573dce38c8289b5b8049db1fc17",
+    "url": "/index.html"
+  }
+];
